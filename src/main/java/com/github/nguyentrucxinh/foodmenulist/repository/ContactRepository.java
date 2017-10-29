1cddc415e06b75a60a37d816b1e86161d51a533d
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-10-29 23:46:09 +0700

    Clean repository

--- src/main/java/com/github/nguyentrucxinh/foodmenulist/repository/ContactRepository.java
+++ src/main/java/com/github/nguyentrucxinh/foodmenulist/repository/ContactRepository.java
@@ -1,7 +1,6 @@
 package com.github.nguyentrucxinh.foodmenulist.repository;
 
 import com.github.nguyentrucxinh.foodmenulist.domain.Contact;
-import com.github.nguyentrucxinh.foodmenulist.repository.ObjectifyGenericRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
