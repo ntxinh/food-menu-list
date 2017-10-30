c5cf72cbec2b2b0bf07923d9ceddfc9ab6a673a7
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-10-30 22:40:30 +0700

    Clean main class

--- src/main/java/com/github/nguyentrucxinh/foodmenulist/Application.java
+++ src/main/java/com/github/nguyentrucxinh/foodmenulist/Application.java
@@ -2,11 +2,18 @@ package com.github.nguyentrucxinh.foodmenulist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(final String... args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
