54c9ab6ac0f3d880dd9afaed85656f7101764862
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-12-24 12:34:17 +0700

    :sparkles: Temp

--- backend/src/main/java/com/github/nguyentrucxinh/foodmenulist/service/backup/ContactServiceImpl.java
+++
@@ -1,9 +0,0 @@
-//package com.github.nguyentrucxinh.foodmenulist.service.impl;
-//
-//import com.github.nguyentrucxinh.foodmenulist.domain.Contact;
-//import com.github.nguyentrucxinh.foodmenulist.service.ContactService;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class ContactServiceImpl extends GenericServiceImpl<Contact> implements ContactService {
-//}
